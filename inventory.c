330e036ac0bb67311f90767b06d68d92642ead78
ZakChebaane <150349051+ZakChebaane@users.noreply.github.com>
2026-08-30 18:09:20 -0500

    made it so you it checks if id allready exists before adding another item with the same ID

--- inventory.c
+++ inventory.c
@@ -2,7 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 
+static int checkID (Item *items, int *count, int id) {
+    for (int i = 0; i < *count; i++) {
+        if (items[i].id == id) {
+            return 0;
+        }
+    }
+    return 1;
+}
 void add (Item *items, int *count, int id, char *name, int qty, double price) {
+    if (checkID(items, count, id) == 0) {
+        printf("[FAILED] Already exists (ID: %d)\n", id);
+        return;
+    }
     items[*count].id = id;
     strncpy(items[*count].name, name, NAME_LEN);
     items[*count].qty = qty;
